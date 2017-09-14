bec099ba9c5ab4ea87f1480abbfbce993d10ba9a
robotan0921 <robotan0921@gmail.com>
2017-09-14 22:01:34 +0900

    Configure build for ARM

--- mruby-1.3.0/build_config.rb
+++ mruby-1.3.0/build_config.rb
@@ -84,57 +84,57 @@ MRuby::Build.new do |conf|
   # conf.enable_bintest
 end
 
-MRuby::Build.new('host-debug') do |conf|
-  # load specific toolchain settings
+# MRuby::Build.new('host-debug') do |conf|
+#   # load specific toolchain settings
 
-  # Gets set by the VS command prompts.
-  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
-    toolchain :visualcpp
-  else
-    toolchain :gcc
-  end
+#   # Gets set by the VS command prompts.
+#   if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+#     toolchain :visualcpp
+#   else
+#     toolchain :gcc
+#   end
 
-  enable_debug
+#   enable_debug
 
-  # include the default GEMs
-  conf.gembox 'default'
+#   # include the default GEMs
+#   conf.gembox 'default'
 
-  # C compiler settings
-  conf.cc.defines = %w(MRB_ENABLE_DEBUG_HOOK)
+#   # C compiler settings
+#   conf.cc.defines = %w(MRB_ENABLE_DEBUG_HOOK)
 
-  # Generate mruby debugger command (require mruby-eval)
-  conf.gem :core => "mruby-bin-debugger"
+#   # Generate mruby debugger command (require mruby-eval)
+#   conf.gem :core => "mruby-bin-debugger"
 
-  # bintest
-  # conf.enable_bintest
-end
+#   # bintest
+#   # conf.enable_bintest
+# end
 
-MRuby::Build.new('test') do |conf|
-  # Gets set by the VS command prompts.
-  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
-    toolchain :visualcpp
-  else
-    toolchain :gcc
-  end
+# MRuby::Build.new('test') do |conf|
+#   # Gets set by the VS command prompts.
+#   if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+#     toolchain :visualcpp
+#   else
+#     toolchain :gcc
+#   end
 
-  enable_debug
-  conf.enable_bintest
-  conf.enable_test
+#   enable_debug
+#   conf.enable_bintest
+#   conf.enable_test
 
-  conf.gembox 'default'
-end
+#   conf.gembox 'default'
+# end
 
-MRuby::Build.new('bench') do |conf|
-  # Gets set by the VS command prompts.
-  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
-    toolchain :visualcpp
-  else
-    toolchain :gcc
-    conf.cc.flags << '-O3'
-  end
+# MRuby::Build.new('bench') do |conf|
+#   # Gets set by the VS command prompts.
+#   if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+#     toolchain :visualcpp
+#   else
+#     toolchain :gcc
+#     conf.cc.flags << '-O3'
+#   end
 
-  conf.gembox 'default'
-end
+#   conf.gembox 'default'
+# end
 
 # Define cross build settings
 # MRuby::CrossBuild.new('32bit') do |conf|
@@ -150,3 +150,103 @@ end
 #   conf.test_runner.command = 'env'
 #
 # end
+
+######################################
+
+MRuby::CrossBuild.new('ARM') do |conf|
+  toolchain :gcc
+  conf.cc.defines = %w(DISABLE_STDIO)
+  conf.bins = []
+
+  [conf.cc, conf.objc, conf.asm].each do |cc|
+    cc.command = ENV['CC'] || 'arm-none-eabi-gcc'
+    cc.flags = [ENV['CFLAGS'] || %w(-g -std=gnu99 -O3 -Wall -Werror-implicit-function-declaration)]
+    cc.include_paths = ["#{MRUBY_ROOT}/include"],
+
+    cc.defines = %w(DISABLE_GEMS)
+    cc.option_include_path = '-I%s'
+    cc.option_define = '-D%s'
+    cc.compile_options = '%{flags} -MMD -o %{outfile} -c %{infile}'
+  end
+
+  [conf.cxx].each do |cxx|
+    cxx.command = ENV['CXX'] || 'arm-none-eabi-g++'
+    cxx.flags = [ENV['CXXFLAGS'] || ENV['CFLAGS'] || %w(-g -O3 -Wall -Werror-implicit-function-declaration)]
+    cxx.include_paths = ["#{MRUBY_ROOT}/include"]
+    cxx.defines = %w(DISABLE_GEMS)
+    cxx.option_include_path = '-I%s'
+    cxx.option_define = '-D%s'
+    cxx.compile_options = '%{flags} -MMD -o %{outfile} -c %{infile}'
+  end
+
+  conf.linker do |linker|
+    linker.command = ENV['LD'] || 'arm-none-eabi-gcc'
+    linker.flags = [ENV['LDFLAGS'] || %w()]
+    linker.libraries = %w(m)
+    linker.library_paths = []
+    linker.option_library = '-l%s'
+    linker.option_library_path = '-L%s'
+    linker.link_options = '%{flags} -o %{outfile} %{objs} %{flags_before_libraries} %{libs} %{flags_after_libraries}'
+  end
+
+  # Archiver settings
+  conf.archiver do |archiver|
+    archiver.command = ENV['AR'] || 'arm-none-eabi-ar'
+p  end
+  #
+  #   conf.cc.flags << "-m32"
+  #   conf.linker.flags << "-m32"
+
+  # Use standard print/puts/p
+  conf.gem :core => "mruby-print"
+  # Use extended toplevel object (main) methods
+  conf.gem :core => "mruby-toplevel-ext"
+  # Use standard Math module
+  #  conf.gem :core => "mruby-math"
+  # Use mruby-compiler to build other mrbgems
+  conf.gem :core => "mruby-compiler"
+  conf.build_mrbtest_lib_only
+=begin
+  # Use standard Kernel#sprintf method
+  conf.gem :core => "mruby-sprintf"
+  # Use standard Time class
+  # conf.gem :core => "mruby-time"
+  # Use standard Struct class
+  conf.gem :core => "mruby-struct"
+  # Use extensional Enumerable module
+  conf.gem :core => "mruby-enum-ext"
+  # Use extensional String class
+  #conf.gem :core => "mruby-string-ext"
+  # Use extensional Numeric class
+  conf.gem :core => "mruby-numeric-ext"
+  # Use extensional Array class
+  conf.gem :core => "mruby-array-ext"
+  # Use extensional Hash class
+  conf.gem :core => "mruby-hash-ext"
+  # Use extensional Range class
+  conf.gem :core => "mruby-range-ext"
+  # Use extensional Proc class
+  conf.gem :core => "mruby-proc-ext"
+  # Use extensional Symbol class
+  conf.gem :core => "mruby-symbol-ext"
+  # Use Random class
+  # conf.gem :core => "mruby-random"
+  # Use extensional Object class
+  conf.gem :core => "mruby-object-ext"
+  # Use ObjectSpace class
+  conf.gem :core => "mruby-objectspace"
+  # Use Fiber class
+  conf.gem :core => "mruby-fiber"
+  # Use Enumerator class (require mruby-fiber)
+  conf.gem :core => "mruby-enumerator"
+  # Use Enumerable::Lazy class (require mruby-enumerator)
+  conf.gem :core => "mruby-enum-lazy"
+  # Generate mirb command
+  # conf.gem :core => "mruby-bin-mirb"
+  # Generate mruby command
+  # conf.gem :core => "mruby-bin-mruby"
+  # Generate mruby-strip command
+  # conf.gem :core => "mruby-bin-strip"
+=end
+
+end
