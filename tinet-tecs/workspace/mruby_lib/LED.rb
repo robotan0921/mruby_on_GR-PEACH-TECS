fea13411a3fafb4884d7d41d57b23308ff6fb676
robotan0921 <robotan0921@gmail.com>
2017-11-21 18:57:09 +0900

    Add mruby_lib and move libs to it

---
+++ tinet-tecs/workspace/mruby_lib/LED.rb
@@ -0,0 +1,16 @@
+class LED < TECS::TsLED
+	@@color_hash = {:user=>12, :red=>13, :green=>14, :blue=>15}
+	private
+	def initialize()
+		super "BridgeLED"
+	end
+	@@instance = self.new
+
+	public
+	def self.color= (clr)
+		@@instance.setColor( @@color_hash[clr] )
+	end
+	def self.off
+		@@instance.off
+	end
+end
---
+++ tinet-tecs/workspace/mruby_lib/RTOS.rb
@@ -0,0 +1,30 @@
+class RTOS < TECS::TsKernel
+	private
+	def initialize()
+		super "BridgeKernel"
+	end
+	@@instance = self.new
+
+	public
+	def self.delay(time)
+		@@instance.delay(time)
+	end
+	def self.msec
+		time = TECS::ULongPointer.new(1)
+		@@instance.getTime(time)
+		time.value
+	end
+	def self.usec
+		time = TECS::ULongPointer.new(1)
+		@@instance.getMicroTime(time)
+		time.value
+	end
+	def self.sleep
+		@@instance.sleep
+	end
+	def self.id
+		id = TECS::IntPointer.new(1)
+		@@instance.getTaskId(id)
+		return id.value
+	end
+end
